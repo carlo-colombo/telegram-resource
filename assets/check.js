e974b6d36b87deace3ab466b50e6c45d56225976
Carlo Colombo <carlo.colombo@gmx.com>
2016-12-05 18:14:27 +0000

    linux does not like multiple arguments in /usr/bin/env

--- assets/check.js
+++ assets/check.js
@@ -2,24 +2,21 @@ const jsonStdin = require('./json-stdin')
 const Api = require('./api-telegram.js')
 
 async function main(){
-  const {
-    source: {filter = ".*", telegram_key, flags},
-    version: {update_id}
-  } = await jsonStdin()
+    const {
+        source: {filter = ".*", telegram_key, flags},
+        version: {update_id}
+    } = await jsonStdin()
 
-  const api = new Api(telegram_key)
+    const api = new Api(telegram_key)
+    const {result} = await api.getUpdates(update_id)
 
-  const {result} = await api.getUpdates(update_id)
-
-  process.stdout.write(JSON.stringify(check(result, new RegExp(filter, flags))))
-
+    process.stdout.write(JSON.stringify(check(result, new RegExp(filter, flags))))
 }
 
 function check(result, filter){
-  return result
-    .filter( ({message: {text}}) => filter.test(text))
-    .map( ({update_id}) => ({update_id}) )
+    return result
+        .filter( ({message: {text}}) => filter.test(text))
+        .map( ({update_id}) => ({update_id}) )
 }
 
- module.exports = main
-
+main()
