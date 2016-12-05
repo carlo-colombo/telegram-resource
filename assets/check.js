bbea96b0d7f2e5fdcc9ff46be69721b0dd6092ee
Pivotal <pcf-rabbitmq@pivotal.io>
2016-12-05 08:49:38 +0000

    removed commented code

--- assets/check.js
+++ assets/check.js
@@ -9,40 +9,6 @@ jq -M  ".result |  map(select(.message.text == '${filter}') | map({ update_id: .
 const jsonStdin = require('./json-stdin')
 const Api = require('./api-telegram.js')
 
-// const request => (path, host, data) => new Promise( (resolve, reject) => {
-//   const req = http.request({
-//     var req = http.request(options, (res) => {
-//       console.log(`STATUS: ${res.statusCode}`);
-//       console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-//       res.setEncoding('utf8');
-//       res.on('data', (chunk) => {
-//             console.log(`BODY: ${chunk}`);
-//           });
-//         res.on('end', () => {
-//               console.log('No more data in response.');
-//             });
-//     });
-
-//     req.on('error', (e) => {
-//         console.log(`problem with request: ${e.message}`);
-//     });
-
-//     // write data to request body
-//     // req.write(postData);
-//     // req.end();
-//   }, (res) => {
-//     console.log(`STATUS: ${res.statusCode}`);
-//     console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-//     let content = ''
-//   });
-
-//   req.on('error', e => reject(e))
-//   return updates.filter(update => !filter || update.text
-//   // write data to request body
-//   // req.write(postData);
-//   // req.end();
-// })
-
 async function main(){
   const {
     source: {filter, telegram_key},
