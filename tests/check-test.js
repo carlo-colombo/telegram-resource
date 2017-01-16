f348bcd99f41bec1adeacf9b9138cb4776bb66e4
Denise Yu <dyu@pivotal.io>
2017-01-16 17:58:47 +0000

    more tests
    Signed-off-by: Carlo Colombo <ccolombo@pivotal.io>

--- tests/check-test.js
+++ tests/check-test.js
@@ -2,7 +2,17 @@ const should = require('should')
 const check = require('../assets/check.js')
 const sinon = require('sinon')
 
+function makeMock(result){
+  return class MockApi{
+    getUpdates(){
+      return {result}
+    }
+  }
+}
+
 describe('check', () => {
+  const jsonConfigStub = sinon.stub().returns({source: {}, version: null})
+
   it('should return a rejected promise if the conf is missing or not valid', done => {
     const main = check(sinon.spy())
 
@@ -11,17 +21,18 @@ describe('check', () => {
   })
 
   it('should return an empty list in case of no message available', async () =>{
-    const jsonConfigStub = sinon.stub().returns({source: {}, version: null})
-
-    class MockApi {
-      getUpdates(){
-        return {result: []}
-      }
-    }
-
-    const main = check(jsonConfigStub, MockApi)
+    const main = check(jsonConfigStub, makeMock([]))
     const res = await main()
 
     return should(res).be.eql([])
+  })
+
+  it('should return an upadte for each message available', async ()=>{
+    const main = check(jsonConfigStub, makeMock([{
+      message: {text: 'hi'}, update_id: 42
+    }]))
+    const res = await main()
+
+    return should(res).be.eql([{update_id: '42'}])
   })
 })
