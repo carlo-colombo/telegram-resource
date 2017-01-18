2ac883277228bbd2e8f4f4768bcba40e8c581aa3
Carlo Colombo <ccolombo@pivotal.io>
2017-01-18 09:57:48 +0000

    tests
    Signed-off-by: Denise Yu <dyu@pivotal.io>

--- tests/check-test.js
+++ tests/check-test.js
@@ -3,58 +3,50 @@ const check = require('../assets/check.js')
 const sinon = require('sinon')
 
 describe('check', () => {
-  // it('should return a rejected promise if the conf is missing or not valid', done => {
-  //   const main = check(sinon.spy())
-
-  //   main().then(done, ()=>done())
-  // })
-
-  describe('with a valid configuration', ()=>{
-    it('returns an empty list in case of no message available', async () =>{
+  describe('with no message available', ()=>{
+    it('returns an empty list', async () =>{
       const res = check({
-        getUpdates: sinon.mock().returns({result: []})
+        getUpdates: () => ({result: []})
       })()
 
       return should(await res).be.eql([])
     })
+  })
 
+  describe('with messages available', ()=>{
+    const mockApi = {
+      getUpdates: () => ({
+        result: [{message: {text: 'hi'}, update_id: 42}]
+      })
+    }
     it('returns an update for each message available', async ()=>{
-      const res = check({
-        getUpdates: sinon.mock().returns({
-          result: [{message: {text: 'hi'}, update_id: 42}]
-        })
-      })()
+      const res = check(mockApi)()
 
       should(await res).be.eql([{update_id: '42'}])
     })
 
-  })
+    describe('with a filter defined', ()=>{
+      const filter = /not_hi/
 
-  describe('with a filter defined', ()=>{
-    const filter = /not_hi/
-    it('filters out messages not matching the regex',  async ()=>{
-      const res = await check({
-        getUpdates: sinon.mock().returns({
-          result: [
-            {message: {text: 'hi'}, update_id: 42},
-            {message: {text: 'not_hi'}, update_id: 43},
-          ]
-        })
-      }, {}, filter)()
+      it('filters out messages not matching the regex',  async ()=>{
+        const res = await check({
+          getUpdates: () => ({
+            result: [
+              {message: {text: 'hi'}, update_id: 42},
+              {message: {text: 'not_hi'}, update_id: 43},
+            ]
+          })
+        }, {}, filter)()
 
-      should(await res).be.eql([{update_id: '43'}])
-    })
+        should(await res).be.eql([{update_id: '43'}])
+      })
 
-    it('filters out messages not matching the regex, even all of them',  async ()=>{
-      const res = check({
-        getUpdates: sinon.mock().returns({
-          result: [
-            {message: {text: 'hi'}, update_id: 42},
-          ]
-        })
-      }, {}, filter)()
+      it('filters out messages not matching the regex, even all of them',  async ()=>{
+        const res = check(mockApi, {}, filter)()
 
-      should(await res).be.eql([])
+        should(await res).be.eql([])
+      })
     })
   })
+
 })
