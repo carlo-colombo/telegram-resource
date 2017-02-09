f2b3bfb8e0b0174bdd8f8df1efe71be5cf9e4949
Carlo Colombo <ccolombo@pivotal.io>
2017-02-09 18:03:25 +0000

    implementing resource

--- assets/out.js
+++ assets/out.js
@@ -1,40 +1,57 @@
 const Api = require('./api-telegram');
 const path = require('path');
-const { kv, readFile, jsonStdin } = require('./utils.js');
 
-async function main(dest) {
-  try {
-    const {
-      params: { chat_id: chat_id_file, text: text_file },
-      source: { telegram_key }
-    } = await jsonStdin();
+async function main(readConfig, jsonStdin, Api, readFile, dest) {
+  const {
+    api,
+    chat_id: chat_id_file,
+    text: text_file
+  } = await readConfig(jsonStdin(), Api);
 
-    const api = new Api(telegram_key);
+  const [chat_id, text] = await Promise.all([
+    readFile(path.join(dest, chat_id_file)),
+    readFile(path.join(dest, text_file))
+  ]);
 
-    console.error('error', telegram_key);
-    const [chat_id, text] = await Promise.all([
-      readFile(path.join(dest, chat_id_file)),
-      readFile(path.join(dest, text_file))
-    ]);
-    console.error('error', chat_id, text);
-    // const {result: {chat}} = await api.sendMessage(chat_id, text)
-    const resp = await api.sendMessage(chat_id, text);
-    console.error('error', resp);
-    const { result: { chat } } = resp;
+  const resp = await api.sendMessage(chat_id, text);
+  // try {
+  //   // console.log('at least tryiing');
+  //   // const {
+  //   //   params: { chat_id: chat_id_file, text: text_file },
+  //   //   source: { telegram_key }
+  //   // } = await jsonStdin();
 
-    process.stdout.write(
-      JSON.stringify({
-        version: {},
-        metadata: [
-          kv('username', chat.username),
-          kv('chat_id', chat.id.toString())
-        ]
-      })
-    );
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
+  //   // const api = new Api(telegram_key);
+
+  //   // console.error('error', telegram_key);
+  //   // const [chat_id, text] = await Promise.all([
+  //   //   readFile(path.join(dest, chat_id_file)),
+  //   //   readFile(path.join(dest, text_file))
+  //   // ]);
+  //   // console.error('error', chat_id, text);
+  //   // // const {result: {chat}} = await api.sendMessage(chat_id, text)
+  //   // console.error('error', resp);
+  const { result: { chat } } = resp;
+
+  return {
+    version: {},
+    metadata: [kv('username', chat.username), kv('chat_id', chat.id.toString())]
+  };
+  // } catch (e) {
+  //   console.error(e);
+  //   return {};
+  // }
+  return await null;
 }
 
-main(process.argv.pop());
+module.exports = {
+  main
+};
+
+const { kv, readFile, jsonStdin } = require('./utils.js');
+
+if (require.main === module) {
+  jsonStdout(
+    main(readConfig, jsonStdin, Api, check, writeFile, process.argv.pop())
+  );
+}
