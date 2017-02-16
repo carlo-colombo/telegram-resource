baffd44e646cfaa71cb973f82b0ad1f66cfc8c99
Carlo Colombo <ccolombo@pivotal.io>
2017-02-16 17:39:56 +0000

    add type to readConfig

--- src/wirings.js
+++ src/wirings.js
@@ -1,11 +1,13 @@
 /* @flow */
 const Api = require('./api-telegram');
-const { jsonStdin, readConfig } = require('./utils.js');
+const { jsonStdin, readConfig: _readConfig } = require('./utils.js');
 
 import type { Configuration, ReadConfiguration } from './types';
 
+const readConfig: ReadConfiguration = async function(): Promise<Configuration> {
+  return await _readConfig(jsonStdin(), Api);
+};
+
 module.exports = {
-  readConfig: async function(): Promise<Configuration> {
-    return await readConfig(jsonStdin(), Api);
-  }
+  readConfig
 };
